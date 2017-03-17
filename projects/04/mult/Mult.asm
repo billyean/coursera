51c315aa7edc9e7e22490303465081ece45e4a52
Haibo Yan <tristan.yim@gmail.com>
2017-03-16 23:22:07 -0700

    Adding project 4 solution

---
+++ projects/04/mult/Mult.asm
@@ -0,0 +1,45 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+
+@i
+M=1
+@R0
+D=M
+@n
+M=D
+@sum
+M=0
+
+(LOOP)
+@i
+D=M
+@n
+D=D-M
+@STOP
+D;JGT
+
+@R1
+D=M
+@sum
+M=D+M
+@i
+M=M+1
+@LOOP
+0;JMP
+
+(STOP)
+@sum
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
